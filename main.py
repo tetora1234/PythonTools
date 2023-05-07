b476b8b8a64f2d8b61adfc638c8e945f2ce83c79
tetora1234 <nidera515@gmail.com>
2023-05-08 01:55:24 +0900

    gpt実行を実装

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import whisper
 import gradio as gr
+import openai
+
+openai.api_key = "sk-123"
 
 
 def AudioFileToText(input):
@@ -18,15 +21,36 @@ def AudioFileToText(input):
     return text
 
 
+def ChatGptAsk(text):
+    """_summary_
+
+    Args:
+        text (sring): gptに送信するテキスト
+
+    Returns:
+        string: gptの結果テキスト
+    """
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": text},
+        ],
+    )
+    return res["choices"][0]["message"]["content"]
+
+
 with gr.Blocks() as Interface:
-    with gr.Tab("音声からテキスト抽出"):
+    with gr.Tab("Whisper"):
         AudioFileTo_input = gr.inputs.Audio(source="upload", type="filepath")
-        AudioFileTo_output = gr.Textbox()
-        AudioFileTo_button = gr.Button("音声からテキスト抽出")
-    with gr.Tab("カモフラージュ"):
-        sample_input = gr.inputs.Audio(source="upload", type="filepath")
+        AudioFileTo_output = gr.Textbox(lines=4, placeholder="抽出テキスト")
+        AudioFileTo_button = gr.Button("Whisper実行")
+    with gr.Tab("ChatGpt"):
+        ChatGPT_input = gr.Textbox(lines=4, placeholder="送信テキスト")
+        ChatGPT_output = gr.Textbox(lines=4, placeholder="返答テキスト")
+        ChatGPT_button = gr.Button("ChatGpt実行")
     AudioFileTo_button.click(
         AudioFileToText, inputs=AudioFileTo_input, outputs=AudioFileTo_output
     )
+    ChatGPT_button.click(ChatGptAsk, inputs=ChatGPT_input, outputs=ChatGPT_output)
 
 Interface.launch()

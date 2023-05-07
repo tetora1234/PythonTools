c048e942614f3279fed82d1b41a18d956d940de3
tetora1234 <nidera515@gmail.com>
2023-05-08 01:13:18 +0900

    gradioのuiを調整

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import whisper
 import gradio as gr
 
-model = whisper.load_model("base")
 
+def AudioFileToText(input):
+    """_summary_
 
-def speechRecognitionModel(input):
-    ## モデル（データセット）読み込み
+    Args:
+        input (array): アップロードされた音声ファイルのbinary配列
+
+    Returns:
+        string:音声から抽出したwhisperのテキストデータ
+    """
     model = whisper.load_model("small")
-
-    ## 音声へのパス
     path = input
-
-    ## 結果を出力と同時に取得
     result = model.transcribe(path, verbose=True, language="ja")
     text = result["text"]
     return text
 
 
-gr.Interface(
-    title="Whisper Speech Recognition Model",
-    fn=speechRecognitionModel,
-    inputs=[gr.inputs.Audio(source="upload", type="filepath")],
-    outputs=["textbox"],
-    live=True,
-).launch()
+with gr.Blocks() as Interface:
+    with gr.Tab("音声からテキスト抽出"):
+        AudioFileTo_input = gr.inputs.Audio(source="upload", type="filepath")
+        AudioFileTo_output = gr.Textbox()
+        AudioFileTo_button = gr.Button("音声からテキスト抽出")
+    with gr.Tab("カモフラージュ"):
+        sample_input = gr.inputs.Audio(source="upload", type="filepath")
+    AudioFileTo_button.click(
+        AudioFileToText, inputs=AudioFileTo_input, outputs=AudioFileTo_output
+    )
+
+Interface.launch()
